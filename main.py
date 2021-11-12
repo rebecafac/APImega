c3b0113297874104af10685292203b6e622f3513
Rebeca Facchinato <32989509+rebecafac@users.noreply.github.com>
2021-11-12 15:47:00 -0300

    Ajustes na API
    - Ajustes de gravação dos dados no banco
    - Criação de método POST para inserir dados
    - Criação de método GET para buscar dados a partir da chave, número do concurso ou data.

--- main.py
+++ main.py
@@ -1,15 +1,56 @@
-from flask import Flask, request
 import fitz
+import json
+import requests
+from flask import Flask, request
+from util import Resultados as Res
+import os
 
-if __name__ == '__main__':
 
+app = Flask(__name__)
+app.debug = True
+
+
+# Função que realiza a requisição de post para
+# gravar os últimos resultados no banco
+@app.route('/Insert', methods=['POST'])
+def insert_resultado():
+    # main()
+    try:
+        data = request.data
+        data = data.decode('utf-8')
+        if data:
+            id_obj = Res().insere_resultado(
+                table='resultados',
+                response=json.loads(data)
+            )
+
+        print(id_obj)
+        return gera_response('', len(id_obj) > 0, 200)
+    except Exception as e:
+        print(e)
+        return gera_response(
+            message='Não foi possível processar a inclusão',
+            data='',
+            status_code=500
+        )
+
+
+def gera_response(message, data, status_code):
+    body = {}
+
+    if message:
+        body['message'] = message
+
+    body['data'] = data
+
+    return body, status_code
+
+def main():
     conteudo = ""
 
     with fitz.open("Arquivo.pdf") as pdf:
-
         for pagina in pdf:
             conteudo += pagina.getText()
-
 
     obJson = {}
     concursos = []
@@ -30,7 +71,6 @@ if __name__ == '__main__':
     qtd_ganhadores = ""
     premio = ""
 
-
     # Captura linha a linha do pdf
     for x in range(5607):
 
@@ -39,13 +79,14 @@ if __name__ == '__main__':
             fim = 47
 
         else:
-            dados = conteudo[fim+1:]
+            dados = conteudo[fim + 1:]
             inicio = fim + 1
             fim = dados.find("\n") + inicio
 
         linha = conteudo[inicio: fim]
 
-        isNotCabec = linha.find('Data') == -1 and linha.find('Gan.') == -1 and linha.find('Todos os resultados da Mega Sena - Rede Loteria') == -1
+        isNotCabec = linha.find('Data') == -1 and linha.find('Gan.') == -1 and linha.find(
+            'Todos os resultados da Mega Sena - Rede Loteria') == -1
 
         # Avalia qual tipo de informação a linha possui se não for linha de cabeçalho
         if isNotCabec:
@@ -70,22 +111,22 @@ if __name__ == '__main__':
             elif linha.find(",") > -1:
                 premio = linha
 
-
             if concurso != '' and data != '' and chave != '':
-                concursos.append(
-                    {"id": index - 1,
-                     "concurso": concurso,
-                     "data": data,
-                     "col1": col1,
-                     "col2": col2,
-                     "col3": col3,
-                     "col4": col4,
-                     "col5": col5,
-                     "col6": col6,
-                     "chave": chave,
-                     "qtd_ganhadores": qtd_ganhadores,
-                     "premio": premio
-                     })
+                index = index - 1
+                concursos.append({"id": index,
+                                  "concurso": concurso,
+                                  "data": data,
+                                  "col1": col1,
+                                  "col2": col2,
+                                  "col3": col3,
+                                  "col4": col4,
+                                  "col5": col5,
+                                  "col6": col6,
+                                  "chave": chave,
+                                  "qtd_ganhadores": qtd_ganhadores,
+                                  "premio": premio
+                                  }
+                                 )
 
                 concurso = ""
                 data = ""
@@ -100,13 +141,11 @@ if __name__ == '__main__':
                 premio = ""
 
 
-print(concursos)
+    print(concursos)
 
-
-
-
-
-
+if "__main__" == __name__:
+   port = int(os.environ.get("PORT", 5000))
+   app.run(host="0.0.0.0", port=port, debug=True)
 
 
 
